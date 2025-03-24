a3b34c12055abe045974a10455f1e81815964f9c
Keitaro Kimura <kyohxyz@gmail.com>
2025-03-24 15:19:34 +0900

    テストコード追加

--- tests/test_get_bookmark.py
+++ tests/test_get_bookmark.py
@@ -91,3 +91,64 @@ class TestGetBookmark(BaseTest):
         assert "tags" not in res_bookmark
         assert res_bookmark["created_at"] == datetime_to_str(db_bookmark.created_at)
         assert res_bookmark["updated_at"] == datetime_to_str(db_bookmark.updated_at)
+
+    def test_get_one_notfound(self, client: TestClient, db_session: SessionForTest):
+        """
+        異常系:
+        ハッシュIDが存在しないブックマーク取得
+        """
+        # テストデータ作成
+        self.create_bookmarks(db_session, num=1)
+
+        hashed_id = "0" * 64
+
+        # リクエストの送信
+        response = client.get(f"/bookmarks/{hashed_id}")
+
+        # レスポンスの検証
+        assert response.status_code == 404
+
+    def test_get_invalid_hashed_id(self, client: TestClient, db_session: SessionForTest):
+        """
+        異常系:
+        ハッシュID不正
+        """
+        # ハッシュIDの長さ不正
+        hashed_id = "0" * 63
+        response = client.get(f"/bookmarks/{hashed_id}")
+        assert response.status_code == 422
+
+        # ハッシュIDのフォーマット不正
+        hashed_id = "invalid_hashed_id"
+        response = client.get(f"/bookmarks/{hashed_id}")
+        assert response.status_code == 422
+
+    def test_get_list_by_tagname_notfound(self, client: TestClient, db_session: SessionForTest):
+        """
+        正常系:
+        存在しないタグ名でブックマークリスト取得
+        """
+        # テストデータ作成
+        self.create_bookmarks(db_session, num=1)
+
+        # リクエストの送信
+        response = client.get("/bookmarks?tag=notfound")
+
+        # レスポンスの検証
+        assert response.status_code == 200
+        response_body = response.json()
+        assert "bookmarks" in response_body
+        assert len(response_body["bookmarks"]) == 0
+
+    def test_get_list_by_tagname_invalid(self, client: TestClient, db_session: SessionForTest):
+        """
+        異常系:
+        タグ名不正
+        """
+        # タグ名の指定なし
+        response = client.get("/bookmarks?tag=")
+        assert response.status_code == 422
+
+        # タグ名がユニークでない
+        response = client.get("/bookmarks?tag=tag1&tag=tag1")
+        assert response.status_code == 422
