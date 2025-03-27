ea77ec51dacda53a4cff8026d1ef625e8b62f195
Keitaro Kimura <kyohxyz@gmail.com>
2025-03-27 19:08:35 +0900

    バージョン取得APIのリファクタリング

--- src/dto/version.py
+++ src/dto/version.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, ConfigDict
 from ..libs.version import APP_VERSION
 
 
-class Version(BaseModel):
+class ResponseForGetVersion(BaseModel):
     version: str
 
     model_config = ConfigDict(json_schema_extra={"examples": [{"version": APP_VERSION}]})
