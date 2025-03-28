b8e7a788fc648bb48838370b007fd169ec53d376
Keitaro Kimura <kyohxyz@gmail.com>
2025-03-28 17:56:03 +0900

    不要コード削除

--- src/libs/util.py
+++ src/libs/util.py
@@ -12,27 +12,6 @@ def get_hashed_id(value: str) -> str:
     return sha256((value + SALT).encode()).hexdigest()
 
 
-def now() -> datetime:
-    """
-    現在日時を返す
-    """
-    return datetime.now()
-
-
-def none_to_empty(s: str | None) -> str:
-    """
-    Noneであれば空文字を返す
-    """
-    return s if s is not None else ""
-
-
-def empty_to_none(s: str) -> str | None:
-    """
-    空文字であればNoneを返す
-    """
-    return s if s != "" else None
-
-
 def str_to_datetime(s: str) -> datetime:
     """
     文字列をdatetime型に変換
